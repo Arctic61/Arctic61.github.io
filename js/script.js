38dc0ee27d7327bb2fde95d48bf4cf449083905e
Arctic61 <mark61gamer@gmail.com>
2025-05-31 12:55:20 -0600

    bugs

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let number1 = 15;
+let number2 = 30;
+console.log(number1+number2);
+// let body = document.querySelector('body')
+// body.innerHTML = "<h1>asdiohgkljsdfg<h1>"
+let btn = document.querySelector('.btn');
+btn.addEventListener('click', function(){
+    alert("asdgdfsdfsdf");
+    btn.innerHTML = "boom";
+})
