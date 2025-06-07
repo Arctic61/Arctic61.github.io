c11f018bd7c6a5ad9436bfe57951dc0e32466aa5
Arctic61 <mark61gamer@gmail.com>
2025-06-07 14:51:32 -0600

    bugs

---
+++ script.js
@@ -0,0 +1,18 @@
+// $('.carousel').slick();
+
+$('.carousel').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+
+// $('.your-class').slick({
+// setting-name: setting-value
+// });
+    
